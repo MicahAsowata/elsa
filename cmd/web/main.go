1d97349959250a66595410872e60d331bb741432
Micah Asowata <andrewgary963@gmail.com>
2023-05-24 10:51:41 +0100

    Removed Gorm and uuid

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 
 	controller "github.com/MicahAsowata/elsa/cmd/web/controllers/task"
-	"github.com/MicahAsowata/elsa/internal/db/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,11 +11,6 @@ const port = ":3000"
 
 func main() {
 	app := fiber.New()
-	db := models.Db()
-	err := db.AutoMigrate(&models.Task{})
-	if err != nil {
-		log.Fatal(err)
-	}
 	controller.TaskRoutes(app)
 	log.Println("Speak, for thy servant heareth")
 	app.Listen(port)
