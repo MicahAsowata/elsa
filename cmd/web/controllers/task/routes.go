2b0e675f2ca1af7d7a2aa8bb7a6f3bfd384f9153
Micah Asowata <andrewgary963@gmail.com>
2023-05-25 17:45:45 +0100

    Grouped the routes properly

--- cmd/web/controllers/task/routes.go
+++ cmd/web/controllers/task/routes.go
@@ -1,19 +1,16 @@
 package controller
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-const baseRouteName = "tarea"
-
 func TaskRoutes(app *fiber.App) {
-	app.Get(fmt.Sprintf("/%s/", baseRouteName), TaskIndex)
-	app.Get(fmt.Sprintf("/%s/new", baseRouteName), TaskNew)
-	app.Post(fmt.Sprintf("/%s/", baseRouteName), TaskCreate)
-	app.Get(fmt.Sprintf("/%s/:id", baseRouteName), TaskShow)
-	app.Get(fmt.Sprintf("/%s/:id/edit", baseRouteName), TaskEdit)
-	app.Post(fmt.Sprintf("/%s/:id", baseRouteName), TaskUpdate)
-	app.Get(fmt.Sprintf("/%s/:id/delete", baseRouteName), TaskDestroy)
+	baseRouteName := app.Group("/tarea")
+	baseRouteName.Get("/new", TaskNew)
+	baseRouteName.Get("/", TaskIndex)
+	baseRouteName.Post("/", TaskCreate)
+	baseRouteName.Get("/:id", TaskShow)
+	baseRouteName.Get("/:id/edit", TaskEdit)
+	baseRouteName.Post("/:id", TaskUpdate)
+	baseRouteName.Get("/:id/delete", TaskDestroy)
 }
