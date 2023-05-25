64631362a801c3bdc5a73026971d748631db6d54
Micah Asowata <andrewgary963@gmail.com>
2023-05-25 18:42:53 +0100

    Added dbx and MySQL driver and re-arranged the directory structure to ease imports

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	controller "github.com/MicahAsowata/elsa/cmd/web/controllers/task"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/pocketbase/dbx"
 )
 
 const port = ":3000"
 
+type application struct {
+	db *dbx.DB
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Could not load env")
+	}
+	hostName := os.Getenv("HOST_NAME")
+	dbName := os.Getenv("DB_NAME")
+	hostPswd := os.Getenv("HOST_PSWD")
+
 	app := fiber.New()
-	controller.TaskRoutes(app)
+	db, err := dbx.Open("mysql", fmt.Sprintf("mysql://%s:%s@tcp(localhost:3306)/%s", hostName, hostPswd, dbName))
+	if err != nil {
+		log.Println("Could not connect with the db")
+	}
+
+	base := application{
+		db: db,
+	}
+	log.Print(base)
 	log.Println("Speak, for thy servant heareth")
 	app.Listen(port)
 
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,4 +1,4 @@
-package controller
+package main
 
 import (
 	"github.com/gofiber/fiber/v2"
